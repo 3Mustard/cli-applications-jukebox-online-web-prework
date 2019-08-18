9c0d596c65816719ed5f6014515119ac3e38cfe0
Matthew C <m23.cullen@gmail.com>
2019-08-18 01:08:53 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -28,7 +28,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   if (1..9).to_a.include?(input.to_i)
-    puts "Playing <#{songs[]}>"
+    puts "Playing <#{songs[input.to_i - 1]}>"
 end
 
 
