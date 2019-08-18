acc48875b40660af3c1b9a956a76d7b5aab94ce8
Matthew C <m23.cullen@gmail.com>
2019-08-18 01:18:28 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -44,15 +44,15 @@ def run
   puts "Pleae enter a command:"
   input = gets.downcase.strip
   case input
-  when "help"
-    help
-  when "list"
-    list(songs)
-  when "play"
-    play(songs)
-  when "exit"
-    exit_jukebox
-  end
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+    end
 end
 
 
