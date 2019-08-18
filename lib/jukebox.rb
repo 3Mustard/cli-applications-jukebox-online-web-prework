f744ad85f128e23468fd56f796336eb049213fee
Matthew C <m23.cullen@gmail.com>
2019-08-18 01:32:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -60,7 +60,7 @@ def run(songs)
         input = gets.chomp
       when "exit"
         exit_jukebox
-        break
+        puts "please enter a command:"
       else
         puts "Invalid input, please try again:"
         input = gets.chomp
